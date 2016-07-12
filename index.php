2dc84dded8ee2f77f6fff504f5f9d37bc2250f25
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-12 14:53:56 -0600

    added  another test table

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 			<h1>Hi World!</h1>
 		</header>
 		<main>
-			<p>Tuxedo cats always looking dapper run in circles. Chase imaginary bugs. Steal the warm chair right after you get up put butt in owner's face but sit by the fire, or knock dish off table head butt cant eat out of my own dish or if it smells like fish eat as much as you wish for stick butt in face. Thug cat groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked! and lick arm hair for intently stare at the same spot, but swat turds around the house. Present belly, scratch hand when stroked missing until dinner time stares at human while pushing stuff off a table poop in the plant pot fall asleep on the washing machine soft kitty warm kitty little ball of furr this human feeds me, i should be a god.
+			<p id="first-paragraph"> <strong>Tuxedo</strong> cats always looking <em>dapper</em> run in circles. Chase imaginary bugs. Steal the warm chair right after you get up put butt in owner's face but sit by the fire, or knock dish off table head butt cant eat out of my own dish or if it smells like fish eat as much as you wish for stick butt in face. Thug cat groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked! and lick arm hair for intently stare at the same spot, but swat turds around the house. Present belly, scratch hand when stroked missing until dinner time stares at human while pushing stuff off a table poop in the plant pot fall asleep on the washing machine soft kitty warm kitty little ball of furr this human feeds me, i should be a god.
 			</p>
-			<h2>About me</h2>
+			<h2 class="pretty-title red-text">About me</h2>
 			<ul>
 				<li>Item 1</li>
 				<li>Item 2</li>
@@ -41,6 +41,48 @@
 					</td>
 					<td>
 						table cell 4
+					</td>
+				</tr>
+			</table>
+			<table>
+				<tr>
+					<th>
+						Table cell 1
+					</th>
+				</tr>
+				<tr>
+					<th>
+						Table cell 2
+					</th>
+				</tr>
+				<tr>
+					<td>
+						Table cell 3
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Table cell 4
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Table cell 5
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Table cell 6
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Table cell 7
+					</td>
+				</tr>
+				<tr>
+					<td>
+						Table cell 8
 					</td>
 				</tr>
 			</table>
