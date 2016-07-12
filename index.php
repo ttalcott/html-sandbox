c5d2d2044130f23ed406fc0b8de2732fd6455489
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-12 13:39:53 -0600

    added table

--- index.php
+++ index.php
@@ -5,41 +5,48 @@
 		<title>My First Practice Web Page</title>
 	</head>
 	<body>
-		<h1>Hi World!</h1>
-		<p>Tuxedo cats always looking dapper run in circles. Chase imaginary bugs. Steal the warm chair right after you get up put butt in owner's face but sit by the fire, or knock dish off table head butt cant eat out of my own dish or if it smells like fish eat as much as you wish for stick butt in face. Thug cat groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked! and lick arm hair for intently stare at the same spot, but swat turds around the house. Present belly, scratch hand when stroked missing until dinner time stares at human while pushing stuff off a table poop in the plant pot fall asleep on the washing machine soft kitty warm kitty little ball of furr this human feeds me, i should be a god.
-		</p>
-		<h2>About me</h2>
-		<ul>
-			<li>Item 1</li>
-			<li>Item 2</li>
-			<li>Item 3</li>
-		</ul>
-		<ol>
-			<li>Item 1</li>
-			<li>Item 2</li>
-			<li>Item 3</li>
-		</ol>
-		<div>
-			<a href="https://google.com" target="_blank">Go to Google.com</a>
-		</div>
-		<img src="images/stormtrooper.jpg" alt="Stormtrooper" />
-		<table>
-			<tr>
-				<th>
-					table cell 1
-				</th>
-				<th>
-					table cell 2
-				</th>
-			</tr>
-			<tr>
-				<td>
-					table cell 3
-				</td>
-				<td>
-					table cell 4
-				</td>
-			</tr>
-		</table>
+		<header>
+			<h1>Hi World!</h1>
+		</header>
+		<main>
+			<p>Tuxedo cats always looking dapper run in circles. Chase imaginary bugs. Steal the warm chair right after you get up put butt in owner's face but sit by the fire, or knock dish off table head butt cant eat out of my own dish or if it smells like fish eat as much as you wish for stick butt in face. Thug cat groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked! and lick arm hair for intently stare at the same spot, but swat turds around the house. Present belly, scratch hand when stroked missing until dinner time stares at human while pushing stuff off a table poop in the plant pot fall asleep on the washing machine soft kitty warm kitty little ball of furr this human feeds me, i should be a god.
+			</p>
+			<h2>About me</h2>
+			<ul>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+			</ul>
+			<ol>
+				<li>Item 1</li>
+				<li>Item 2</li>
+				<li>Item 3</li>
+			</ol>
+			<div>
+				<a href="https://google.com" target="_blank">Go to Google.com</a>
+			</div>
+			<img src="images/stormtrooper.jpg" alt="Stormtrooper" />
+			<table>
+				<tr>
+					<th>
+						table cell 1
+					</th>
+					<th>
+						table cell 2
+					</th>
+				</tr>
+				<tr>
+					<td>
+						table cell 3
+					</td>
+					<td>
+						table cell 4
+					</td>
+				</tr>
+			</table>
+		</main>
+		<footer>
+			&copy; 2016
+		</footer>
 	</body>
 </html>
